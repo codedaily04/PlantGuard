aec75926cd9f88a64644ac1e255392808e7f518c
Abhishek Nishad <codedaily04@gmail.com>
2026-09-06 18:24:54 +0530

    refactor(telemetry): consolidate readings under machine telemetry

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,6 @@ const machineRoutes = require("./routes/machineRoutes");
 const factoryRoutes = require("./routes/factoryRoutes");
 const sensorRoutes = require("./routes/sensorRoutes");
 const plantRoutes = require("./routes/plantRoutes");
-const plantSensorRoutes = require("./routes/plantSensorRoutes");
 const aiRoutes = require("./routes/aiRoutes");
 const app = express();
 
@@ -23,6 +22,5 @@ app.use("/api/machines", machineRoutes);
 app.use("/api/factories", factoryRoutes);
 app.use("/api/sensors", sensorRoutes);
 app.use("/api/plants", plantRoutes);
-app.use("/api/plant-sensors", plantSensorRoutes);
 app.use("/api/ai", aiRoutes);
 module.exports = app;
